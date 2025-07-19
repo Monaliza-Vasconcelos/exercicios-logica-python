b7279df1dd92ecdd7d08138a6c835e5b2259a18d
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-07-19 14:43:03 -0300

    Delete ex006-dobro-triplo-raizquadrada.py

--- operadores-aritmeticos/ex006-dobro-triplo-raizquadrada.py
+++
@@ -1,7 +0,0 @@
-N = float(input('Digite um número: '))
-dobro  = N*2
-triplo = N*3
-raizqua = N**2
-print('Dobro de {} é {}'.format(N,dobro))
-print('Triplo de {} é {}'.format(N,triplo))
-print('Raiz quadrada de {} é {}'.format(N,raizqua))
