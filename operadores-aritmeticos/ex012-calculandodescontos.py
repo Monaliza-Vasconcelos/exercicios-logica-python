3d7bf78985cb8e3ae3eccb31bae194ae7c2ebee3
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-07-19 14:43:20 -0300

    Delete ex012-calculandodescontos.py

--- operadores-aritmeticos/ex012-calculandodescontos.py
+++
@@ -1,4 +0,0 @@
-preco = float(input('Qual o valor do produto? '))
-descont = float(preco*5/100)
-total_a_pagar = preco-descont
-print('O total a pagar com 5% de desconto é R${}'.format(total_a_pagar))
