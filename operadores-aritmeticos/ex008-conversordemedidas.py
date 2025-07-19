788ab6e6456c1bce7cf255e102b9556e8b2d7b9a
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-07-19 14:43:07 -0300

    Delete ex008-conversordemedidas.py

--- operadores-aritmeticos/ex008-conversordemedidas.py
+++
@@ -1,5 +0,0 @@
-valor_em_metros = float(input('Digite um valor em metros: '))
-valor_em_cm = float(valor_em_metros*100)
-valor_em_mm = float(valor_em_metros*1000)
-print('Valor em centimetros {}'.format(valor_em_cm))
-print('Valor em milimetros {}'.format(valor_em_mm))
