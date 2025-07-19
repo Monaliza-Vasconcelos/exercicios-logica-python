3de7174b15ec5575cf482360f1b9b7221a0df06e
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-07-19 14:42:19 -0300

    Create ex014-conversordetemperaturas.py

---
+++ 03 - operadores-aritmeticos/ex014-conversordetemperaturas.py
@@ -0,0 +1,3 @@
+temperatura_em_c = float(input('Informe a temperatura em ºC: '))
+Fahrenheit = float(temperatura_em_c*1.8+32)
+print('A temperatura em {}ºC corresponde a {}ºF'.format(temperatura_em_c,Fahrenheit))
