99639d69c3ff9c53c76a602549f8e1de6f254bf3
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-07-25 15:58:03 -0300

    Delete ex013-reajustesalarial.py

--- 02 - operadores-aritmeticos/ex013-reajustesalarial.py
+++
@@ -1,4 +0,0 @@
-salario = float(input('Qual seu salário? '))
-reajuste = float(salario*15/100)
-valor_com_aumento = float(salario+reajuste)
-print('Seu novo salário com aumento de 15% é de: R${}'.format(valor_com_aumento))
